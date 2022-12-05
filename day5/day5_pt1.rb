3b9aeb19bfb7bd7c9f022e69978212d36eaa32c9
Luis Urraca <luis.urraca@zendesk.com>
2022-12-05 23:14:14 +0000

    Day 5 - Part 1 solution

---
+++ day5/day5_pt1.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+
+input = File.open('input.txt').read
+
+def parse_crate_line(line:, size:)
+  ar = []
+  size.times do |i|
+    ar << line.slice((4*(i+1)) - 3)
+  end
+  ar
+end
+
+def parse_starting_stack(input:)
+  starting_stack_hash = {}
+  starting_stack = []
+
+  input.each_line do |line|
+    break if line == "\n"
+    starting_stack << line
+  end
+
+  starting_stack_hash.tap do |h|
+    starting_stack.last.strip.split.each do |s|
+      h[s] = []
+    end
+  end
+  *crates, last = starting_stack
+
+  stacks = last.strip.split
+
+  crates.each do |c|
+    parse_crate_line(line: c.chop, size: stacks.size).each_with_index do |cc, i|
+      starting_stack_hash[(i+1).to_s] = [cc, *starting_stack_hash[(i+1).to_s]] - [" ", nil]
+    end
+  end
+  starting_stack_hash
+end
+
+def parse_moves(move:)
+  mv = move.split
+  [mv[1].to_i, mv[3], mv[5]]
+end
+
+passed_headers = false
+
+starting_stack = parse_starting_stack(input: input)
+
+input.each_line do |line|
+  if line == "\n"
+    passed_headers = true
+    next
+  end
+  next unless passed_headers
+  p = parse_moves(move: line)
+
+  p[0].times do
+    c = starting_stack[p[1]].pop
+    starting_stack[p[2]] << c
+  end
+end
+
+
+starting_stack.each do |k,v|
+  p v.last
+end
